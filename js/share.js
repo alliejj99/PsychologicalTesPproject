012766d2932a4500d64dab6429eac6deeaa70058
alliejj99 <118407356+alliejj99@users.noreply.github.com>
2022-12-19 19:48:54 +0900

    16강

--- js/share.js
+++ js/share.js
@@ -1,4 +1,4 @@
-const url = 'https://twelvelovetype.netlify.app/';
+const url = 'https://psychologicaltestproject.netlify.app/';
 
 function setShare(){
   var resultImg = document.querySelector('#resultImg');
